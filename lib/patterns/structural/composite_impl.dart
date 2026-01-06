39725bb910b7729bf33aff3747ec8770026b9188
rinaeshin <rinaeshin@gmail.com>
2026-01-06 12:10:15 +0900

    fix: avoid_print in composite_impl

--- lib/patterns/structural/composite_impl.dart
+++ lib/patterns/structural/composite_impl.dart
@@ -1,22 +1,24 @@
 // Composite example
 abstract class Component {
-  void operation();
+  String operation();
 }
 
 class Leaf implements Component {
   final String name;
   Leaf(this.name);
   @override
-  void operation() => print('Leaf $name');
+  String operation() => 'Leaf $name';
 }
 
 class Composite implements Component {
   final List<Component> children = [];
   @override
-  void operation() {
+  String operation() {
+    final results = <String>[];
     for (var c in children) {
-      c.operation();
+      results.add(c.operation());
     }
+    return results.join(', ');
   }
 
   void add(Component c) => children.add(c);
